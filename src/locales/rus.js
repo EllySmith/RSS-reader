189d7179dda2889928a30a4fa74e9f4b9c4ba96b
Alexandra Adelson <adelson.alexandra@gmail.com>
2023-07-18 03:42:30 +0200

    i18n library added

---
+++ src/locales/rus.js
@@ -0,0 +1,7 @@
+export default {
+  translation: {
+    title: 'Создайте свой RSS поток',
+    addRSS: 'Добавить',
+    placeholder: 'Введите ссылку',
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,49 +1,77 @@
 import './styles.scss';
 import 'bootstrap';
 import * as yup from 'yup';
-
-const state = {
-  articleCount: 0,
-  articles:
-     [],
-};
+import i18n from 'i18next';
+import rus from './locales/rus.js';
 
 const app = () => {
-  const field = document.getElementById('link-input');
-  field.focus();
-  field.addEventListener('input', () => {
-    const inputElement = document.getElementById('link-input');
-    const validationSchema = yup.object().shape({
-      rssLink: yup.string().url().required(),
-    });
-    const rssLink = inputElement.value.trim();
-    validationSchema.validate({ rssLink }).then(() => {
-      inputElement.classList.remove('invalid');
-    }).catch(() => {
-      inputElement.classList.add('invalid');
-    });
+  const state = {
+    articleCount: 0,
+    articles:
+       [],
+  };
+
+  i18n.init({
+    lng: 'ru',
+    resources: {
+      ru: {
+        translation: rus.translation,
+      },
+    },
   });
+  i18n.changeLanguage('ru');
 
-  const form = document.getElementById('input-form');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const inputElement = document.getElementById('link-input');
-    const result = inputElement.value;
-    const newElement = document.createElement('p');
-    newElement.textContent = `${result}`;
-    const container = document.querySelector('body');
+  const render = () => {
+    const header = document.createElement('h1');
+    const mainContainer = document.querySelector('body');
+    header.textContent = `${i18n.t('title')}`;
+    mainContainer.prepend(header);
 
-    const existingArticle = state.articles.find((article) => article.title === result);
-    if (existingArticle) {
-      inputElement.classList.add('invalid');
-      return;
-    }
+    const field = document.getElementById('link-input');
+    field.focus();
+    field.addEventListener('input', () => {
+      const inputElement = document.getElementById('link-input');
+      const validationSchema = yup.object().shape({
+        rssLink: yup.string().url().required(),
+      });
+      const rssLink = inputElement.value.trim();
+      validationSchema.validate({ rssLink }).then(() => {
+        inputElement.classList.remove('invalid');
+      }).catch(() => {
+        inputElement.classList.add('invalid');
+      });
+    });
 
-    container.append(newElement);
-    state.articles.push({ title: `${result}`, body: '' });
-    inputElement.value = '';
-    console.log(state);
-  });
+    const submitButton = document.getElementById('submit-button');
+    submitButton.textContent = `${i18n.t('addRSS')}`;
+
+    const form = document.getElementById('input-form');
+    const formInputField = document.getElementById('link-input');
+    formInputField.setAttribute('placeholder', `${i18n.t('placeholder')}`);
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const inputElement = document.getElementById('link-input');
+      const result = inputElement.value;
+      const title = document.createElement('p');
+      const newElement = document.createElement('div');
+      newElement.append(title);
+      newElement.classList.add('link-container');
+      title.textContent = `${result}`;
+      const container = document.querySelector('body');
+
+      const existingArticle = state.articles.find((article) => article.title === result);
+      if (existingArticle) {
+        inputElement.classList.add('invalid');
+        return;
+      }
+
+      container.append(newElement);
+      state.articles.push({ title: `${result}`, body: '' });
+      inputElement.value = '';
+    });
+  };
+
+  render(state);
 };
 
 app();
