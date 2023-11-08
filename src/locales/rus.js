3619522cecf2c832a6ec1f13a0d8694582457d44
Alexandra Adelson <adelson.alexandra@gmail.com>
2023-11-08 12:15:13 +0100

    attribute fix

--- src/locales/rus.js
+++ src/locales/rus.js
@@ -7,6 +7,7 @@ export default {
     entrieslisttitle: 'Посты',
     readmore: 'Читать',
     example: 'Пример RSS ссылки: http://example.com/rss/',
+    rssloaded: 'RSS успешно загружен',
     error: {
       notanrss: 'Это не RSS поток.',
       exists: 'Этот поток уже добавлен.',
