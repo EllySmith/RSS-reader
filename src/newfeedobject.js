b723c5d87d0755f75d65752450316bcae1af3aa4
Alexandra Adelson <adelson.alexandra@gmail.com>
2023-11-14 16:28:53 +0100

    not an rss error message fix

--- src/newfeedobject.js
+++ src/newfeedobject.js
@@ -2,6 +2,19 @@ import fetchInfo from './fetchers.js';
 
 const newFeed = async (state, rsslink) => {
   const allOriginsUrl = `https://allorigins.hexlet.app/get?url=${encodeURIComponent(rssLink)}`;
+  const response = await fetch(allOriginsUrl);
+
+  if (!response.ok) {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = `${i18n.t('error.notanrss')}`;
+  }
+
+  const contentType = response.headers.get('content-type'); 
+  if (!isXmlContentType(contentType)) {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = `${i18n.t('error.notanrss')}`;
+  }
+
   const newtitle = await fetchInfo(allOriginsUrl, 'title');
   const newentries = await fetchInfo(allOriginsUrl, 'entries');
   const newdescription = await fetchInfo(allOriginsUrl, 'description');
