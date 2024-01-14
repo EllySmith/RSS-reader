c189fe99fe9f48e8f2491b70a53d6c2e58f852ef
Alexandra Adelson <adelson.alexandra@gmail.com>
2024-01-14 17:09:08 +0100

    error fix

--- src/app.js
+++ src/app.js
@@ -118,10 +118,7 @@ const app = () => {
         if (error.message === 'Invalid URL') {
           watchedState.form.error = 'notalink';
           watchedState.form.valid = false;
-        } else if (error.message === 'Feed not recognized as RSS 1 or 2.') {
-          watchedState.form.error = 'notanrss';
-          watchedState.form.valid = true;
-        } else if (error.message === 'Load failed') {
+        } else {
           watchedState.form.error = 'notanrss';
           watchedState.form.valid = true;
         }
